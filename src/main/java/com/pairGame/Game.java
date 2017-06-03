b7e2da933072ef2f02bdae5224385404b6542364
Aceysx <aceysx@163.com>
2017-06-03 13:58:50 +0800

    添加初始化坐标finish

--- src/main/java/com/pairGame/Game.java
+++ src/main/java/com/pairGame/Game.java
@@ -22,13 +22,21 @@ public class Game {
     }
 
     public static Board initGame() {
+        String[] points;
+        String[] reg = new String[2];
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("请输入棋盘大小:(例如：10,10)");
-        String[] reg = scanner.nextLine().split(",");
-
-        System.out.println("请输入存活的细胞的坐标点，长度小于" + reg[0] + ",宽度小于" + reg[1] + "输入样例：1,1 2,3 4,5");
-        String[] points = scanner.nextLine().split(" ");
+        System.out.println("是(y)否(n)使用默认初始化的坐标");
+        if ("y".equals(scanner.nextLine())) {
+            points = Constant.INIT_POINTS.split(" ");
+            reg[0] = "50";
+            reg[1] = "50";
+        } else {
+            System.out.println("请输入棋盘大小:(例如：10,10)");
+            reg = scanner.nextLine().split(",");
+            System.out.println("请输入存活的细胞的坐标点，长度小于" + reg[0] + ",宽度小于" + reg[1] + "输入样例：1,1 2,3 4,5");
+            points = scanner.nextLine().split(" ");
+        }
 
         System.out.println("请输入需要间隔切换的时间（毫秒）");
         Constant.INTERVAL_TIME = Integer.parseInt(scanner.nextLine());
