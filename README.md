4c662dba4d103417ae9a7ef2f7961b01f4285259
Aceysx <aceysx@163.com>
2017-06-03 15:17:08 +0800

    add REMADE.md file

---
+++ README.md
@@ -0,0 +1,25 @@
+#### 游戏说明
+
+##### 一、操作指南
+
+**1、运行 `Game.java`  中的 `main` 方法**
+
+**2、根据控制台的提示进行输入操作即可**
+
+##### 二、模式说明
+
+- 默认模式：用户只需要选择动画切换时间（毫秒）即可
+- 自定义模式：用户需要输入游戏矩阵的大小，以及细胞活着的坐标和动画切换的时间
+
+##### 三、常量说明
+
+ - public static int INTERVAL_TIME = 1000; //默认的动画切换时间
+ - public static String PANEL_TITLE = "GAME"; //默认的窗口名
+ - public static int CHESS_SIZE = 8; //默认的细胞大小
+ - public static int INIT_LOCATION_POINT = 8;//默认距离顶点坐标的位置（x，y）
+ - public static int INCREMENT_DISTANCE = 8;//细胞之间的间隔
+ - public static int ALIVE_NUMBER = 3;//存活的情况下周围的细胞数量
+ - public static int NO_CHANGE_NUMBER = 2;//当前细胞状态不变的情况下周围的细胞数量
+ 
+ - public static String INIT_POINTS = ""; //默认的细胞坐标点集
+ - public static String INIT_SIZE = "";//默认的矩阵大小
--- src/main/java/com/pairGame/Constant.java
+++ src/main/java/com/pairGame/Constant.java
@@ -7,12 +7,12 @@ public class Constant {
 
     public static int INTERVAL_TIME = 1000;
     public static String PANEL_TITLE = "GAME";
-    public static int CHESS_SIZE = 15;
-    public static int INIT_LOCATION_POINT = 15;
-    public static int INCREMENT_DISTANCE = 15;
+    public static int CHESS_SIZE = 8;
+    public static int INIT_LOCATION_POINT = 8;
+    public static int INCREMENT_DISTANCE = 8;
     public static int ALIVE_NUMBER = 3;
     public static int NO_CHANGE_NUMBER = 2;
 
-    public static String INIT_POINTS = "11,20 12,20 13,20 14,20 15,20 16,20 17,20 18,20 19,20 24,20 25,20 26,20 27,20 28,20 29,20 30,20 31,20 32,20 33,20 39,20 40,20 41,20";
-    public static String INIT_SIZE = "50";
+    public static String INIT_POINTS = "60,45 61,45 62,45 63,45 64,45 65,45 66,45 67,45 68,45 69,45 70,45 71,45 72,45 73,45 74,45 75,45 76,45 77,45 78,45 79,45 80,45 81,45 82,45 83,45 84,45 85,45 86,45 87,45 88,45 89,45 90,45 91,45 92,45 93,45 94,45 95,45 96,45 97,45 98,45 99,45 100,45";
+    public static String INIT_SIZE = "200";
 }
