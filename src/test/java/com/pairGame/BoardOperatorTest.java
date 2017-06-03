909abce3fa071b8eb2ca8f3232ba75005a6ffa6a
Aceysx <aceysx@163.com>
2017-06-03 13:24:33 +0800

    pass testBoardStateChange

--- src/test/java/com/pairGame/BoardOperatorTest.java
+++ src/test/java/com/pairGame/BoardOperatorTest.java
@@ -1,6 +1,7 @@
 package com.pairGame;
 
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,12 +9,24 @@ import org.junit.Test;
  * Created by acey on 17-6-3.
  */
 public class BoardOperatorTest {
-    Board board ;
+    private Board board ;
+
     @Before
     public void setUp() {
-        board = new Board(10, 10);
-        board.initBoard("1,2 1,3 1,2 5,6 5,7 5,5".split(" "));
+        board = new Board(5, 5);
+        board.initBoard("3,2 3,1 3,3".split(" "));
     }
 
+
+    @Test
+    public void testBoardStateChange() {
+        int[][] except = new int[7][7];
+        except[2][2] = 1;
+        except[3][2] = 1;
+        except[4][2] = 1;
+
+        int[][] actual = BoardOperator.boardStateChange(board.getCurrBoard());
+        Assert.assertArrayEquals(except,actual);
+    }
 
 }
